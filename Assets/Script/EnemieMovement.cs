bf8163c229c12c1496ee25a167f258d60c95f5cb
DESKTOP-NIDTR2H\Dong <5104dong@gmail.com>
2025-11-28 23:51:09 +0700

    add charactor

---
+++ Assets/Script/EnemieMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemieMovement : MonoBehaviour
+{
+    public float speed = 2f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "SwordLv1")
+        {
+            Debug.Log("Enemy đánh Player!");
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Animator animator;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveDirection = moveInput;
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+
+        // Set the "Speed" parameter in the animator
+        animator.SetFloat("Speed", moveDirection.magnitude);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "SwordLv1 (Enemies)")
+        {
+            Debug.Log("Player đánh Enemy!");
+        }
+    }
+}
