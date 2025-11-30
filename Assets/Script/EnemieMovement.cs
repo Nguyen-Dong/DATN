e894ae3c4b80650bb792d4a3918e3dd9641dfe65
DESKTOP-NIDTR2H\Dong <5104dong@gmail.com>
2025-11-30 21:40:30 +0700

    loi animator

--- Assets/Script/EnemieMovement.cs
+++ Assets/Script/EnemieMovement.cs
@@ -3,17 +3,41 @@
 public class EnemieMovement : MonoBehaviour
 {
     public float speed = 2f;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        // Only move if speed is greater than 0
+        if (speed > 0)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            anim.SetInteger("State", 2);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Use CompareTag for better performance and flexibility.
+        // Make sure your Player GameObject has the "Player" tag.
         if (collision.gameObject.name == "SwordLv1")
         {
-            Debug.Log("Enemy đánh Player!");
+            Debug.Log("Enemy attacks Player!");
+            speed = 0f; // Stop movement
+            anim.SetTrigger("Attack"); // Trigger the Attack animation
         }
+    }
+
+    public void SetHead()
+    {
+        // Thêm logic bạn muốn thực thi ở đây.
+       // Ví dụ, để kiểm tra, bạn có thể thêm một dòng debug:
+        Debug.Log("SetHead event was called!");
     }
 }
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,34 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    private Animator animator;
-    private Vector2 moveInput;
+    public float speed = 2f;
+    private Rigidbody2D rb;
+    private Animator anim;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    private void Update()
+    void Update()
     {
-        Vector2 moveDirection = moveInput;
-        transform.Translate(moveDirection * speed * Time.deltaTime);
-
-        // Set the "Speed" parameter in the animator
-        animator.SetFloat("Speed", moveDirection.magnitude);
-    }
-
-    public void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
+        // Only move if speed is greater than 0
+        if (speed > 0)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            anim.SetInteger("State", 2);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "SwordLv1 (Enemies)")
+        
+        if (collision.gameObject.name == "SwordLv1 (Enimies)")
         {
-            Debug.Log("Player đánh Enemy!");
+            Debug.Log("Player attacks Enemy!");
+            speed = 0f; // Stop movement
+            anim.SetTrigger("Attack"); 
         }
     }
+
 }
